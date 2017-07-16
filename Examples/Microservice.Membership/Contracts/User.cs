363d91e8b4ea0cea7ce818b2d1931844c6b118bc
Diamond-Dragon <diamond_dragon@tut.by>
2017-07-16 16:44:25 +0300

    User microservice added

---
+++ Examples/Microservice.Membership/Contracts/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Microservice.Membership.Contracts
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ Examples/Microservice.Membership/Contracts/UserCollection.cs
@@ -0,0 +1,8 @@
+﻿namespace Microservice.Membership.Contracts
+{
+    public class UserCollection
+    {
+        public User[] Items { get; set; }
+        public int Count { get; set; }
+    }
+}
