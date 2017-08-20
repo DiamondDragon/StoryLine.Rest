e4291daec23f43bb1f94d741ef2c019894892e20
Andrei Salanoi <diamond_dragon@tut.by>
2017-08-20 17:07:30 +0300

    Fix for GetText() when response body is empty. This behavior can be controlled now. (#10)

---
+++ StoryLine.Rest.Tests/Extensions/ResponseExtensionsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using StoryLine.Exceptions;
+using StoryLine.Rest.Extensions;
+using StoryLine.Rest.Services.Http;
+using Xunit;
+
+namespace StoryLine.Rest.Tests.Extensions
+{
+    public class ResponseExtensionsTests
+    {
+        private const string BodyText = "Dragon12345";
+
+        private readonly Response _response;
+
+        public ResponseExtensionsTests()
+        {
+            _response = new Response
+            {
+                Status = 200,
+                Body = Encoding.UTF8.GetBytes(BodyText),
+                Request = new Request
+                {
+                    Service = "Crm",
+                    Method = "GET",
+                    Url = "/v1/test"
+                }
+            };
+        }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(199)]
+        [InlineData(300)]
+        [InlineData(400)]
+        public void GetText_When_Request_Validation_Enabled_And_Status_Invalid_And_Empty_Text_Should_Throw_Expectation_Exception(int status)
+        {
+            _response.Status = status;
+
+            Assert.Throws<ExpectationException>(() => _response.GetText());
+        }
+
+        [Fact]
+        public void GetText_When_Request_Validation_Enabled_And_Empty_Text_Should_Throw_Expectation_Exception()
+        {
+            _response.Body = new byte[0];
+
+            Assert.Throws<ExpectationException>(() => _response.GetText());
+        }
+
+        [Theory]
+        [InlineData(100)]
+        [InlineData(199)]
+        [InlineData(300)]
+        [InlineData(400)]
+        public void GetText_When_Request_Validation_Disabled_And_Status_Invalid_And_Empty_Text_Should_Return_Expected_Body(int status)
+        {
+            _response.Status = status;
+
+            _response.GetText(false, false).Should().Be(BodyText);
+        }
+
+        [Fact]
+        public void GetText_When_Request_Validation_Disabled_And_Empty_Text_Should_Return_Empty_Body()
+        {
+            _response.Body = new byte[0];
+
+            _response.GetText(false, false).Should().BeEmpty();
+        }
+    }
+}
--- StoryLine.Rest/Extensions/ResponseExtensions.cs
+++ StoryLine.Rest/Extensions/ResponseExtensions.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using System.Text;
+using StoryLine.Exceptions;
 using StoryLine.Rest.Services.Http;
 
 namespace StoryLine.Rest.Extensions
 {
     public static class ResponseExtensions
     {
-        public static string GetText(this IResponse response)
+        public static string GetText(this IResponse response, bool failOnEmptyBody = true, bool failOnNonSuccessResponse = true)
         {
             if (response == null)
                 throw new ArgumentNullException(nameof(response));
 
+            if (failOnEmptyBody && (response.Body == null || response.Body.Length == 0))
+                throw new ExpectationException("Response has empty body. Details: " + GetRequestDetails(response));
+
+            if (failOnNonSuccessResponse && (response.Status < 200 || response.Status >= 300))
+                throw new ExpectationException("Response status is not successful. Details: " + GetRequestDetails(response));
+
             return Config.ResponseToTextConverter.GetText(response);
+        }
+
+        private static string GetRequestDetails(IResponse response)
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("Service=" + response.Request.Service + ", ");
+            builder.Append("Method=" + response.Request.Method + ", ");
+            builder.Append("Url=" + response.Request.Url + ", ");
+            builder.Append("Status=" + response.Status);
+
+            return builder.ToString();
         }
     }
 }
