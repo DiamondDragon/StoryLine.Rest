ed2e4a43d31b80f5d229b208b2653e6c9c04a255
Diamond-Dragon <diamond_dragon@tut.by>
2017-10-08 17:36:18 +0300

    Response serializer added

---
+++ StoryLine.Rest.Tests/Loggers/TestJsonResponseLogger.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using StoryLine.Rest.Loggers;
+using StoryLine.Rest.Services.Http;
+using Xunit;
+
+namespace StoryLine.Rest.Tests.Loggers
+{
+    public class TestJsonResponseLogger
+    {
+        private readonly JsonResponseLogger _underTest;
+        private readonly MemoryStream _output;
+
+        public TestJsonResponseLogger()
+        {
+            _output = new MemoryStream();
+
+            _underTest = new JsonResponseLogger(_output);
+        }
+
+        [Fact]
+        public void Add_Should_Save_Response_As_Expected_Json()
+        {
+            _underTest.Add(CreateResponse());
+            _underTest.Add(CreateResponse());
+
+            GetOutputText().Should().Be("[{\"request\":{\"service\":\"Srv1\",\"method\":\"POST\",\"url\":\"/v1/test/srv\",\"headers\":{\"header1\":[\"value1\"],\"header2\":[\"value2\"]},\"body\":\"UmVxdWVzdEJvZHk=\",\"properties\":{}},\"exception\":null,\"headers\":{\"header3\":[\"value3\"],\"header4\":[\"value4\"]},\"body\":\"UmVzcG9uc2VCb2R5\",\"status\":200,\"reasonPhrase\":\"Phrase1\",\"properties\":{\"key1\":\"prop1\"}},{\"request\":{\"service\":\"Srv1\",\"method\":\"POST\",\"url\":\"/v1/test/srv\",\"headers\":{\"header1\":[\"value1\"],\"header2\":[\"value2\"]},\"body\":\"UmVxdWVzdEJvZHk=\",\"properties\":{}},\"exception\":null,\"headers\":{\"header3\":[\"value3\"],\"header4\":[\"value4\"]},\"body\":\"UmVzcG9uc2VCb2R5\",\"status\":200,\"reasonPhrase\":\"Phrase1\",\"properties\":{\"key1\":\"prop1\"}}]");
+        }
+
+        private static Response CreateResponse()
+        {
+            return new Response
+            {
+                Request = new Request
+                {
+                    Body = Encoding.UTF8.GetBytes("RequestBody"),
+                    Headers = new Dictionary<string, string[]>
+                    {
+                        ["header1"] = new[] { "value1" },
+                        ["header2"] = new[] { "value2" }
+                    },
+                    Method = "POST",
+                    Service = "Srv1",
+                    Url = "/v1/test/srv"
+                },
+                Headers = new Dictionary<string, string[]>
+                {
+                    ["header3"] = new[] { "value3" },
+                    ["header4"] = new[] { "value4" }
+                },
+                Body = Encoding.UTF8.GetBytes("ResponseBody"),
+                ReasonPhrase = "Phrase1",
+                Status = 200,
+                Properties = new Dictionary<string, object>
+                {
+                    ["key1"] = "prop1"
+                }
+            };
+        }
+
+        private string GetOutputText()
+        {
+            _underTest.Dispose();
+
+            return Encoding.UTF8.GetString(_output.ToArray());
+        }
+    }
+}
---
+++ StoryLine.Rest/Loggers/JsonResponseLogger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using StoryLine.Rest.Services.Http;
+
+namespace StoryLine.Rest.Loggers
+{
+    public sealed class JsonResponseLogger : IResponseLogger, IDisposable
+    {
+        public static readonly JsonSerializerSettings DefaultSerializerSettings = new JsonSerializerSettings
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+            NullValueHandling = NullValueHandling.Include
+        };
+
+        private readonly JsonTextWriter _jsonWriter;
+        private readonly JsonSerializer _jsonSerializer;
+
+        public JsonResponseLogger(string filePath)
+            : this(filePath, DefaultSerializerSettings)
+        {
+        }
+
+        public JsonResponseLogger(string filePath, JsonSerializerSettings settings)
+            : this(File.OpenWrite(filePath), settings)
+        {
+        }
+
+        public JsonResponseLogger(Stream outputStream)
+            : this(outputStream, DefaultSerializerSettings)
+        {
+        }
+
+        public JsonResponseLogger(Stream outputStream, JsonSerializerSettings settings)
+        {
+            if (outputStream == null)
+                throw new ArgumentNullException(nameof(outputStream));
+            if (settings == null)
+                throw new ArgumentNullException(nameof(settings));
+
+            _jsonWriter = new JsonTextWriter(new StreamWriter(outputStream));
+            _jsonWriter.WriteStartArray();
+
+            _jsonSerializer = JsonSerializer.Create(settings);
+        }
+
+        public void Add(IResponse response)
+        {
+            if (response == null)
+                throw new ArgumentNullException(nameof(response));
+
+            _jsonSerializer.Serialize(_jsonWriter, response);
+        }
+
+        public void Dispose()
+        {
+            _jsonWriter.WriteEndArray();
+            _jsonWriter.Flush();
+            _jsonWriter.Close();
+        }
+    }
+}
